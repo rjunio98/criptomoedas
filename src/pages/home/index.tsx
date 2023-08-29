e35d5ae855f01ccad0cf9346d3e9df11309d2421
rjunio98 <robledorjunior1@gmail.com>
2023-08-28 23:37:33 -0300

    criando lista de moedas

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,55 @@
 import styles from './home.module.css'
 
+import { Link } from 'react-router-dom'
+
+import { BiSearch } from 'react-icons/bi'
+
 
 export function Home(){
     return (
-        <div className={styles.container}>
-            <h1>Página Home</h1>
-        </div>
-)}
+        <main className={styles.container}>
+            <form className={styles.form}>
+                <input placeholder='Digite o simbolo da moeda: BTC...' />
+
+                <button type='submit'>
+                    <BiSearch size={30} color="#fff"/>
+                </button>
+            </form>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th scope='col'>Moeda</th>
+                        <th scope='col'>Valor mercado</th>
+                        <th scope='col'>Preço</th>
+                        <th scope='col'>Volume</th>
+                    </tr>
+                </thead>
+
+                <tbody id='tbody'>
+                    <tr className={styles.tr}>
+                        <td className={styles.tdLabel}>
+                            <Link to="/detail/btc">
+                                <span className={styles.link}>Bitcoin</span> | BTC
+                            </Link>
+                        </td>
+
+                        <td className={styles.tdLabel}>
+                            R$ 1928
+                        </td>
+
+                        <td className={styles.tdLabel}>
+                            R$ 40.5985
+                        </td>
+
+                        <td className={styles.tdProfit}>
+                            <span>-5.4</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </main>
+    )
+}
